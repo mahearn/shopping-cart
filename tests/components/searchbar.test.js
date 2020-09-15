754acd6dd5825adfc67c9345b33565b43d766f47
Michael Ahearn <mahearnpad@gmail.com>
2020-09-15 13:15:11 +1000

    removed untracked files in gitignore from cache

--- tests/components/searchbar.test.js
+++
@@ -1,7 +0,0 @@
-// __tests__/SearchBar.js
-import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
-
-test('calls out to external api and returns data in json format', async () => {
-    
-})
